149884c3724742a988f5ae8af556dccb746d9e30
adhigame989 <adhigame989@gmail.com>
2026-07-22 16:09:27 +0700

    Update indodax.py

--- api/indodax.py
+++ api/indodax.py
@@ -1,18 +1,21 @@
 import requests
+import time
 
 BASE_URL = "https://indodax.com/api"
-
 
 class IndodaxAPI:
 
     def __init__(self):
         self.session = requests.Session()
 
-    def get_ticker(self, pair):
+        self.cache = {}
+        self.last_update = 0
+
+    def update(self):
 
         try:
 
-            url = f"{BASE_URL}/ticker/{pair}"
+            url = f"{BASE_URL}/ticker_all"
 
             r = self.session.get(url, timeout=10)
 
@@ -20,22 +23,33 @@ class IndodaxAPI:
 
             data = r.json()
 
-            return {
-                "success": True,
-                "last": float(data["ticker"]["last"]),
-                "buy": float(data["ticker"]["buy"]),
-                "sell": float(data["ticker"]["sell"]),
-                "high": float(data["ticker"]["high"]),
-                "low": float(data["ticker"]["low"]),
-                "vol_idr": float(data["ticker"]["vol_idr"])
-            }
+            self.cache = data["tickers"]
+
+            self.last_update = time.time()
+
+            return True
 
         except Exception as e:
 
-            return {
-                "success": False,
-                "error": str(e)
-            }
+            print("API ERROR:", e)
+
+            return False
+
+    def get_ticker(self, pair):
+
+        if pair not in self.cache:
+            return None
+
+        ticker = self.cache[pair]
+
+        return {
+            "last": float(ticker["last"]),
+            "buy": float(ticker["buy"]),
+            "sell": float(ticker["sell"]),
+            "high": float(ticker["high"]),
+            "low": float(ticker["low"]),
+            "vol_idr": float(ticker["vol_idr"])
+        }
 
 
 api = IndodaxAPI()
