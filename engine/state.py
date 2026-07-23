f02a22e696a378fdf44b8efa3fc3d55d58dc8b47
adhigame989 <adhigame989@gmail.com>
2026-07-23 21:23:55 +0700

    Create state.py

---
+++ engine/state.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class BotState(Enum):
+
+    STANDBY = "STANDBY"
+
+    WAIT_ENTRY = "WAIT_ENTRY"
+
+    BUYING = "BUYING"
+
+    HOLDING = "HOLDING"
+
+    TP_ZONE = "TP_ZONE"
+
+    TRAILING = "TRAILING"
+
+    SELLING = "SELLING"
+
+    FINISHED = "FINISHED"
+
+    PAUSED = "PAUSED"
