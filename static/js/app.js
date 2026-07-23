fc5301178c6b6a8f83e317824bb43ca5a2a40011
adhigame989 <adhigame989@gmail.com>
2026-07-23 21:59:27 +0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,21 @@
-console.log("IEB Dashboard Ready");
+async function updateDashboard(){
+
+    try{
+
+        const res = await fetch("/api/status");
+
+        const data = await res.json();
+
+        console.log(data);
+
+    }catch(e){
+
+        console.log(e);
+
+    }
+
+}
+
+updateDashboard();
+
+setInterval(updateDashboard,2000);
