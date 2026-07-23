c97615abf7236ecae8fbfbaf1d24b02a62db162d
adhigame989 <adhigame989@gmail.com>
2026-07-23 22:24:40 +0700

    Create order.py

---
+++ engine/order.py
@@ -0,0 +1,53 @@
+import time
+
+
+class OrderEngine:
+
+    def buy(self, coin, price, capital):
+
+        print("=" * 40)
+        print("BUY ORDER")
+        print("=" * 40)
+
+        print(f"Coin    : {coin}")
+        print(f"Price   : {price}")
+        print(f"Capital : {capital}")
+
+        time.sleep(1)
+
+        print("BUY SUCCESS (SIMULATION)")
+
+        return {
+
+            "success": True,
+
+            "price": price,
+
+            "capital": capital
+
+        }
+
+
+    def sell(self, coin, price):
+
+        print("=" * 40)
+        print("SELL ORDER")
+        print("=" * 40)
+
+        print(f"Coin  : {coin}")
+        print(f"Price : {price}")
+
+        time.sleep(1)
+
+        print("SELL SUCCESS (SIMULATION)")
+
+        return {
+
+            "success": True,
+
+            "price": price
+
+        }
+
+
+order = OrderEngine()
