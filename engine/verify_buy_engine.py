9bddf54aba78add993cb058429465b1c36db139a
adhigame989 <adhigame989@gmail.com>
2026-07-25 15:35:26 +0700

    Create verify_buy_engine.py

---
+++ engine/verify_buy_engine.py
@@ -0,0 +1,67 @@
+from engine.state import BotState
+from engine.order import order
+from core.position_manager import position_manager
+
+
+class VerifyBuyEngine:
+
+    def run(self, engine):
+
+        print("=" * 40)
+        print("VERIFY BUY ENGINE")
+        print("=" * 40)
+
+        verify = order.verify_buy(
+
+            engine.coin,
+
+            engine.order_id
+
+        )
+
+        if not verify["success"]:
+
+            print(
+                verify.get(
+                    "message",
+                    "VERIFY BUY FAILED"
+                )
+            )
+
+            return
+
+        if not verify["filled"]:
+
+            print("WAIT BUY FILL")
+
+            return
+
+        engine.buy_price = float(verify["price"])
+
+        engine.highest_price = engine.buy_price
+
+        qty = float(verify["qty"])
+
+        position_manager.add(
+
+            coin=engine.coin,
+
+            buy_price=engine.buy_price,
+
+            capital=engine.capital,
+
+            qty=qty
+
+        )
+
+        print("=" * 40)
+        print("BUY VERIFIED")
+        print(f"Coin : {engine.coin}")
+        print(f"Buy  : {engine.buy_price:,.0f}")
+        print(f"Qty  : {qty}")
+        print("=" * 40)
+
+        engine.state = BotState.HOLDING
+
+
+verify_buy_engine = VerifyBuyEngine()
