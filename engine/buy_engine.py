757ee6f3212ba3b976cdda48aa527325a22d7cd9
adhigame989 <adhigame989@gmail.com>
2026-07-25 15:34:34 +0700

    Create buy_engine.py

---
+++ engine/buy_engine.py
@@ -0,0 +1,55 @@
+import time
+
+from engine.state import BotState
+from engine.order import order
+from core.wallet_manager import wallet_manager
+
+
+class BuyEngine:
+
+    def run(self, engine):
+
+        print("=" * 40)
+        print("BUY ENGINE")
+        print("=" * 40)
+
+        # Cek saldo IDR
+        if not wallet_manager.can_buy(engine.capital):
+
+            print("BUY FAILED : Insufficient IDR Balance")
+
+            engine.last_buy_failed = time.time()
+
+            engine.state = BotState.WAIT_ENTRY
+
+            return
+
+        # Kirim Buy Order
+        result = order.buy(
+
+            engine.coin,
+
+            engine.entry_price,
+
+            engine.capital
+
+        )
+
+        if result["success"]:
+
+            engine.order_id = result["order_id"]
+
+            print(f"BUY ORDER CREATED : {engine.order_id}")
+
+            engine.state = BotState.VERIFY_BUY
+
+        else:
+
+            print(result.get("message", "BUY FAILED"))
+
+            engine.last_buy_failed = time.time()
+
+            engine.state = BotState.WAIT_ENTRY
+
+
+buy_engine = BuyEngine()
