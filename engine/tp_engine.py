109e17ccb7f339c3e4c0f42a35b82008c4538915
adhigame989 <adhigame989@gmail.com>
2026-07-27 13:28:06 +0700

    Update tp_engine.py

--- engine/tp_engine.py
+++ engine/tp_engine.py
@@ -2,7 +2,6 @@ from api.indodax import api
 
 from engine.state import BotState
 
-
 class TPEngine:
 
     def run(self, engine):
@@ -16,24 +15,25 @@ class TPEngine:
 
         # Update Highest Price
         if engine.current_price > engine.highest_price:
-
             engine.highest_price = engine.current_price
 
         # Hitung Trailing Price
-        engine.trailing_price = engine.highest_price * (
-            1 - (engine.trailing_gap / 100)
+        engine.trailing_price = (
+            engine.highest_price *
+            (1 - (engine.trailing_gap / 100))
         )
 
         print("=" * 40)
-        print("TP ZONE")
+        print("TARGET PRICE ACTIVE")
         print("=" * 40)
 
         print(f"Coin      : {engine.coin}")
+        print(f"Target    : {engine.target_price:,.0f}")
         print(f"Current   : {engine.current_price:,.0f}")
         print(f"Highest   : {engine.highest_price:,.0f}")
         print(f"Trailing  : {engine.trailing_price:,.0f}")
 
-        # Aktifkan mode trailing
+        # Aktifkan trailing
         engine.tp_activated = True
 
         engine.state = BotState.TRAILING
