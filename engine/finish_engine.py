1e1dda12db2a6884373c1949d07efa959d825116
adhigame989 <adhigame989@gmail.com>
2026-07-30 00:22:04 +0700

    Update finish_engine.py

--- engine/finish_engine.py
+++ engine/finish_engine.py
@@ -1,6 +1,5 @@
 from engine.state import BotState
 
-
 class FinishEngine:
 
     def run(self, engine):
@@ -11,27 +10,32 @@ class FinishEngine:
 
         print(f"Trade Finished : {engine.coin}")
 
-        # Reset Trading Variable
+        # Reset order
         engine.order_id = None
         engine.sell_order_id = None
 
+        # Reset price
         engine.buy_price = 0
         engine.sell_price = 0
-
         engine.current_price = 0
         engine.entry_price = 0
-
         engine.highest_price = 0
         engine.trailing_price = 0
 
+        # Reset position
         engine.qty = 0
         engine.buy_time = None
-
         engine.tp_activated = False
 
-        engine.state = BotState.WAIT_ENTRY
+        # Reset trade id
+        engine.trade_id = None
 
-        print("READY FOR NEXT TRADE")
+        # Jika verify_sell_engine sudah meng-load trade berikutnya,
+        # state sudah WAIT_ENTRY.
+        # Kalau tidak ada trade lagi, kembali ke STANDBY.
+        if engine.coin is None:
+            engine.state = BotState.STANDBY
 
+        print("ENGINE RESET COMPLETE")
 
 finish_engine = FinishEngine()
